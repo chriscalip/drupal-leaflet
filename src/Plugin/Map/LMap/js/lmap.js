e9c191e7ec3d8acf7fc796d1124487b5a5e25698
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2015-07-22 22:49:33 +0200

    Take in account the Views settings when creating the map.

--- src/Plugin/Map/LMap/js/lmap.js
+++ src/Plugin/Map/LMap/js/lmap.js
@@ -1,7 +1,7 @@
 Drupal.leaflet.pluginManager.register({
   fs: 'leaflet.Map:LMap',
   init: function(data) {
-    return new L.map(data.opt.target, {zoomControl:false}).setView([0, 0], 1);
+    return new L.map(data.opt.target, {zoomControl:false}).setView(data.opt.view.center, data.opt.view.zoom);
   },
   attach: function(context, settings) {},
   detach: function(context, settings) {}
